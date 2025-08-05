a1d6ac7a6abf1e05fda65f2fcdb3aa690d01eea7
Alexander Gladysh <agladysh@gmail.com>
2025-08-05 20:52:05 +0300

    feat(plugin-jp): shell wrapper for JMSEPath's jp
    Don't ask why shell. We'll see if we'll port to pure JS

---
+++ packages/plugin-jp/src/index.ts
@@ -0,0 +1,26 @@
+import { Readable } from 'node:stream';
+import { fail, spawnText } from '@or-q/lib';
+import type { Plugin } from '@or-q/lib';
+import pkg from '../package.json' with { type: 'json' };
+
+const plugin: Plugin = {
+  name: pkg.name,
+  commands: {
+    jp: {
+      description: "run JMSEPath's jp command",
+      run: async (
+        input: string | Readable,
+        args: string[]
+      ): Promise<string | Readable> => {
+        const query = args.pop();
+        // Lazy, this should be enforced by caller, including usage.
+        if (query === undefined) {
+          fail('usage: jp "[JMSEPath query string]"');
+        }
+        return spawnText('jp', input, { args: [query] });
+      },
+    },
+  },
+};
+
+export default plugin;

67b00d0e817a6aa983a93616bd1e2b21339c19be
Alexander Gladysh <agladysh@gmail.com>
2025-08-05 20:53:23 +0300

    feat: working plugin loading and data flow

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,4 +1,49 @@
 #! /usr/bin/env node --experimental-strip-types --disable-warning=ExperimentalWarning
-import { dummy } from '@or-q/core';
+import { Readable } from 'node:stream';
+import { loadAllPlugins } from '@or-q/core';
+import { fail } from '@or-q/lib';
 
-process.stdout.write(`or-q: ${dummy} NOT IMPLEMENTED\n`);
+async function readableToString(readable: Readable): Promise<string> {
+  let result = '';
+  // Readable supports async iteration out-of-the-box in Node 18+
+  for await (const chunk of readable) {
+    // chunk is a Buffer; convert to string as you go
+    result += chunk.toString('utf8');
+  }
+  return result;
+}
+
+async function main() {
+  const plugins = await loadAllPlugins();
+  const { commands } = plugins;
+
+  const args = process.argv.slice(2);
+  if (args.length === 0) {
+    // Lazy: should write full help
+    process.stdout.write(
+      `Available commands: ${Object.keys(commands).join(', ')}\n`
+    );
+    return;
+  }
+
+  let input: string | Readable = '';
+
+  while (args.length > 0) {
+    const command = args.shift()!;
+    if (!(command in commands)) {
+      fail(`Unknown command ${command}`);
+    }
+    // Lazy, implicit arg eating sucks.
+    input = await commands[command].run(input, args); // This will eat more args
+  }
+
+  if (input instanceof Readable) {
+    input = await readableToString(input);
+  }
+
+  process.stdout.write(input.endsWith('\n') ? input : `${input}\n`);
+}
+
+main().catch((e: unknown) => {
+  console.error('Unexpected error:', e);
+});
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,56 @@
-export const dummy = 42;
+import type { Plugin, Commands } from '@or-q/lib';
+import installedNodeModules from 'installed-node-modules';
+
+export function listAllPlugins(
+  re: RegExp = /((^@or-q\/plugin-)|(or-q-plugin))/
+) {
+  const allPackages = installedNodeModules();
+  const result = [...allPackages].filter((id) => !!re.test(id));
+  return result;
+}
+
+export interface Plugins {
+  names: string[];
+  commands: Commands;
+}
+
+export async function loadAllPlugins(): Promise<Plugins> {
+  const pluginNames = listAllPlugins();
+  const plugins = (
+    await Promise.all(pluginNames.map((name) => import(name)))
+  ).map((module) => module.default as Plugin);
+
+  // Lazy. Optimizable, do not walk over the same array multiple times.
+
+  function resolveRecord<
+    F extends keyof Plugin,
+    K extends keyof Plugin[F],
+    V extends Plugin[F][K],
+    R extends Record<K, V>,
+  >(field: F): R {
+    const result: R = {} as R;
+    for (const plugin of plugins) {
+      if (plugin[field] !== undefined) {
+        for (const [k, v] of Object.entries(plugin[field])) {
+          if (v === undefined) {
+            continue;
+          }
+          if (k in result) {
+            // Lazy. We should capture original setter for more user-friendly error messages.
+            process.emitWarning(
+              `plugin ${plugin.name} overrode previously set ${field} value ${k}`
+            );
+          }
+          result[k as K] = v;
+        }
+      }
+    }
+    return result;
+  }
+
+  // Lazy. This should be typed.
+  return {
+    names: pluginNames,
+    commands: resolveRecord('commands'),
+  };
+}

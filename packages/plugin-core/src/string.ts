625ae4b8d6484166eed20187d78ddd354c0dc71e
Alexander Gladysh <agladysh@gmail.com>
2025-08-08 02:32:56 +0300

    feat(core): replace

--- packages/plugin-core/src/string.ts
+++ packages/plugin-core/src/string.ts
@@ -1,4 +1,5 @@
 import {
+  commandArgument,
   readableToString,
   type Arguments,
   type Commands,
@@ -27,6 +28,27 @@ const commands: Commands = {
     ): Promise<string | Readable> => {
       input = await readableToString(input);
       return JSON.stringify(input);
+    },
+  },
+  replace: {
+    description: 'replaces substring with new string in input',
+    run: async (
+      input: string | Readable,
+      args: Arguments,
+      runtime: IPluginRuntime
+    ): Promise<string | Readable> => {
+      const from = await commandArgument(
+        runtime,
+        args.shift(),
+        `usage: replace "<from>" "<to>"`
+      );
+      const to = await commandArgument(
+        runtime,
+        args.shift(),
+        `usage: replace "<from>" "<to>"`
+      );
+      input = await readableToString(input);
+      return input.replaceAll(from, to);
     },
   },
 };

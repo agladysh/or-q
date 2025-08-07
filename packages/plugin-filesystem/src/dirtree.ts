45e3e17b499801fd87397517953a55f5586c03a2
Alexander Gladysh <agladysh@gmail.com>
2025-08-08 02:17:51 +0300

    feat(filesystem): dirtree-json, dirtree

---
+++ packages/plugin-filesystem/src/dirtree.ts
@@ -0,0 +1,64 @@
+import {
+  readableToString,
+  type Arguments,
+  type Commands,
+  type IPluginRuntime,
+} from '@or-q/lib';
+import yaml from 'yaml';
+import type { Readable } from 'node:stream';
+import path from 'node:path';
+import treeify, { type TreeObject } from 'treeify';
+
+type Dirs = { [dir: string]: Dirs | string | null };
+
+function paths2object(paths: string[]) {
+  const root: Dirs = {};
+  for (const filepath of paths) {
+    const parsed = path.parse(filepath);
+    let base = root;
+    if (parsed.dir !== '') {
+      const dirs = parsed.dir.split(path.sep);
+      for (let dir of dirs) {
+        dir = dir + '/';
+        if (!(dir in base)) {
+          const subdir = {};
+          base[dir] = subdir;
+        }
+        base = base[dir] as Dirs;
+      }
+    }
+    base[parsed.base] = null;
+  }
+  return root;
+}
+
+const commands: Commands = {
+  ['dirtree-json']: {
+    description: 'consumes list of paths, returns hierarchy as JSON',
+    run: async (
+      input: string | Readable,
+      _args: Arguments,
+      _runtime: IPluginRuntime
+    ): Promise<string | Readable> => {
+      input = await readableToString(input);
+      const paths = yaml.parse(input).sort();
+      return JSON.stringify(paths2object(paths), null, 2);
+    },
+  },
+  ['dirtree']: {
+    description: 'consumes list of paths, returns hierarchy as text',
+    run: async (
+      input: string | Readable,
+      _args: Arguments,
+      _runtime: IPluginRuntime
+    ): Promise<string | Readable> => {
+      input = await readableToString(input);
+      const paths = yaml.parse(input).sort();
+      return (
+        '.\n' + treeify.asTree(paths2object(paths) as TreeObject, true, true)
+      );
+    },
+  },
+};
+
+export default commands;
--- packages/plugin-filesystem/src/index.ts
+++ packages/plugin-filesystem/src/index.ts
@@ -1,10 +1,11 @@
 import { mergeCommands, type Plugin } from '@or-q/lib';
 import pkg from '../package.json' with { type: 'json' };
+import dirtree from './dirtree.ts';
 import glob from './glob.ts';
 
 const plugin: Plugin = {
   name: pkg.name,
-  commands: mergeCommands(pkg.name, [glob]),
+  commands: mergeCommands(pkg.name, [dirtree, glob]),
 };
 
 export default plugin;

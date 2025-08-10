c731e6d942788cf30cbef411bb743122e721cd03
Alexander Gladysh <agladysh@gmail.com>
2025-08-10 17:22:49 +0300

    feat(conversation): temperature

--- packages/plugin-openrouter-api/src/conversation.ts
+++ packages/plugin-openrouter-api/src/conversation.ts
@@ -2,6 +2,7 @@ import {
   type Arguments,
   commandArgument,
   type Commands,
+  fail,
   type IPluginRuntime,
   readableToString,
 } from '@or-q/lib';
@@ -65,6 +66,39 @@ const commands: Commands = {
   tool: {
     description: 'appends tool message to an conversation object in input',
     run: messageHandler('tool', 'tool'),
+  },
+  temperature: {
+    description: 'changes conversation object temperature',
+    run: async (
+      input: string | Readable,
+      args: Arguments,
+      runtime: IPluginRuntime
+    ): Promise<string | Readable> => {
+      const usage = 'usage: temperature "<number in [0..2]>"';
+      const temperatureStr: string = await commandArgument(
+        runtime,
+        args.shift(),
+        usage
+      );
+      if (Number.isNaN(Number(temperatureStr))) {
+        return fail(
+          `temperature: expected number in [0..2], got ${temperatureStr} (${typeof temperatureStr})`
+        );
+      }
+      const temperature = Number(temperatureStr);
+      if (temperature < 0 || temperature > 2) {
+        return fail(
+          `temperature: expected number in [0..2], got ${temperature}`
+        );
+      }
+
+      // Lazy. Should validate input and argument
+      const conversation = JSON.parse(await readableToString(input));
+
+      conversation.temperature = temperature;
+
+      return JSON.stringify(conversation, null, 2);
+    },
   },
 };
 

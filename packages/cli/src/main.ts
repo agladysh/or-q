8b4a69147d216048f6d04ba9ba09bfa6a3b68beb
Alexander Gladysh <agladysh@gmail.com>
2025-08-14 02:58:30 +0300

    refactor(cli): modernized code a bit

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,25 +1,21 @@
 #! /usr/bin/env node --env-file-if-exists=.env --experimental-strip-types --disable-warning=ExperimentalWarning
 import { PluginRuntime } from '@or-q/core';
 import { readableToString } from '@or-q/lib';
-import { Readable } from 'node:stream';
+import type { Readable } from 'node:stream';
 
 async function main() {
-  const plugins = await PluginRuntime.fromNodeModules();
+  const runtime = await PluginRuntime.fromNodeModules();
 
   const args = process.argv.slice(2);
   if (args.length === 0) {
     // Lazy: should write full help
-    process.stdout.write(`${plugins.usage()}\n`);
+    process.stdout.write(`${runtime.usage()}\n`);
     return;
   }
 
   let input: string | Readable = '';
 
-  input = await plugins.runCommands(input, args);
-
-  if (input instanceof Readable) {
-    input = await readableToString(input);
-  }
+  input = await readableToString(await runtime.runCommands(input, args));
 
   input = input.trimEnd();
   if (input !== '') {

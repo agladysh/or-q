cb893d2d333d84be4140a235e5499ed302680976
Alexander Gladysh <agladysh@gmail.com>
2025-08-13 20:10:51 +0300

    feat(plugins) dot-or-q-assets

---
+++ packages/plugin-dot-or-q-assets/src/index.ts
@@ -0,0 +1,11 @@
+import { loadAssets, type Plugin } from '@or-q/lib';
+import pkg from '../package.json' with { type: 'json' };
+import path from 'node:path';
+
+const plugin: Plugin = {
+  name: pkg.name,
+  // Lazy. This should support normal hierarchy of .or-q directories (upwards, home, XDG, system, etc.)
+  assets: loadAssets(path.join(process.cwd(), '.or-q/assets/')),
+};
+
+export default plugin;

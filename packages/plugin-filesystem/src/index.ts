e8a3cfceb3060980ebd10a6547b61869034cd4bb
Alexander Gladysh <agladysh@gmail.com>
2025-08-14 00:05:03 +0300

    chore(filesystem): renamed cat.ts to file.ts for sanity

--- packages/plugin-filesystem/src/index.ts
+++ packages/plugin-filesystem/src/index.ts
@@ -1,13 +1,13 @@
 import { mergeCommands, type Plugin } from '@or-q/lib';
 import pkg from '../package.json' with { type: 'json' };
-import cat from './cat.ts';
+import file from './file.ts';
 import dirtree from './dirtree.ts';
 import glob from './glob.ts';
 import ignore from './ignore.ts';
 
 const plugin: Plugin = {
   name: pkg.name,
-  commands: mergeCommands(pkg.name, [cat, dirtree, glob, ignore]),
+  commands: mergeCommands(pkg.name, [file, dirtree, glob, ignore]),
 };
 
 export default plugin;

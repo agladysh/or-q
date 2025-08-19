b5f49a96e1b4f34fb7075546ad783072241dd1c0
Alexander Gladysh <agladysh@gmail.com>
2025-08-19 13:38:19 +0300

    refactor(yaml-script): using resolveAssetSubdir

--- packages/plugin-yaml-script/src/index.ts
+++ packages/plugin-yaml-script/src/index.ts
@@ -5,7 +5,7 @@ import {
   loggingEventName,
   logLevels,
   readableToString,
-  resolveAsset,
+  resolveAssetSubdir,
   type Arguments,
   type IPluginRuntime,
   type Plugin,
@@ -14,8 +14,8 @@ import { parse } from 'node:path';
 import type { Readable } from 'node:stream';
 import yaml from 'yaml';
 
-import pkg from '../package.json' with { type: 'json' };
 import parseArgsStringToArgv from 'string-argv';
+import pkg from '../package.json' with { type: 'json' };
 
 type CommandList = unknown;
 
@@ -281,21 +281,7 @@ const plugin: Plugin = {
       run: async (input: string | Readable, args: Arguments, runtime: IPluginRuntime): Promise<string | Readable> => {
         const uri = await commandArgument(runtime, args.shift(), 'usage: run "<file>"');
 
-        let yamlString = resolveAsset(runtime, uri);
-        if (yamlString === undefined) {
-          // Unqualified URIs are loaded from plugins.
-          // Use dot notation (./dir/filename.yaml) or an absolute path (/path/to/filename.yaml) to load from filesystem
-          // Lazy. This will likely fail on Windows. Normalize plugin path separators on ingestion.
-          const assetNames = assetGlob(runtime, `plugin:*/**/scripts/**/${uri}.yaml`).sort();
-          const assetName = assetNames[0]; // We handle not found below.
-          yamlString = runtime.assets[assetName];
-          if (assetNames.length > 1) {
-            process.emitWarning(
-              `run: several scripts found for "${uri}":\n* ${assetNames.join('\n* ')}\nUsing "${assetName}"`
-            );
-          }
-        }
-
+        const yamlString = resolveAssetSubdir(runtime, uri, 'scripts');
         if (yamlString === undefined) {
           return fail(`run: "${uri}" not found, did you mean "./${uri}?"`);
         }

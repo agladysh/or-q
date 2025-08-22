b985fc173b49b41efe75746ed649286ed0aeb866
Alexander Gladysh <agladysh@gmail.com>
2025-08-22 20:57:31 +0300

    feat(core): rem

--- packages/plugin-core/src/debug.ts
+++ packages/plugin-core/src/debug.ts
@@ -8,6 +8,13 @@ const commands: Commands = {
       return `${JSON.stringify(args)}\n`;
     },
   },
+  rem: {
+    description: 'consumes all remaining arguments and replaces input with them in JSON',
+    run: async (_input: string | Readable, args: Arguments, _runtime: IPluginRuntime): Promise<string | Readable> => {
+      const consumed = args.splice(0, args.length);
+      return `${JSON.stringify(consumed)}\n`;
+    },
+  },
 };
 
 export default commands;

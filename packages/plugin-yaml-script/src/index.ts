9a0080fa437f369e85470cede3ecad186446a718
Alexander Gladysh <agladysh@gmail.com>
2025-08-23 00:29:17 +0300

    refactor(yaml-script): radically simplified _JSON, _DATA and _JSON are no longer full commands
    - yaml-script now depends on @or-q/plugin-core
    - fixed package.json dependencies
    - added tests

--- packages/plugin-yaml-script/src/index.ts
+++ packages/plugin-yaml-script/src/index.ts
@@ -26,120 +26,6 @@ interface Script {
   commands: CommandList;
 }
 
-type JSONPrimitive = string | number | boolean;
-type JSONArray = (JSONPrimitive | JSONRecord | JSONArray)[];
-type JSONRecord = { [key: string]: JSONPrimitive | JSONArray | JSONRecord };
-type JSONValue = JSONArray | JSONRecord | JSONPrimitive;
-
-// Intentionally not handling any keys except _RAW as special
-// Lazy. Seems obsurdly slow. Probably isn't for how it would be used, but is there a simpler solution?
-function loadYAMLAsJSONCommand(stream: Arguments, obj: JSONValue): Arguments {
-  if (typeof obj === 'string') {
-    stream.push('string', String(obj));
-    return stream;
-  }
-
-  if (typeof obj === 'boolean') {
-    stream.push(obj ? 'true' : 'false');
-    return stream;
-  }
-
-  if (!Number.isNaN(Number(obj))) {
-    stream.push('number', String(obj));
-    return stream;
-  }
-
-  if (Array.isArray(obj)) {
-    stream.push('array');
-    for (const item of obj) {
-      loadYAMLAsJSONCommand(stream, item);
-    }
-    stream.push('end-array');
-    return stream;
-  }
-
-  const entries = Object.entries(obj);
-
-  if (entries.length === 1 && entries[0][0] === '_RAW') {
-    stream.push(loadCommands(entries[0][1] as CommandList)); // Lazy. Check schema.
-    return stream;
-  }
-
-  stream.push('object');
-  for (const [key, value] of entries) {
-    stream.push(key);
-    loadYAMLAsJSONCommand(stream, value);
-  }
-  stream.push('end-object');
-
-  return stream;
-}
-
-async function loadInputFromJSONCommand(runtime: IPluginRuntime, args: Arguments): Promise<JSONValue> {
-  // Spammy even for spam, but useful for debugging.
-  /*/
-  // Lazy. Should be a wrapper in the plugin lib.
-  runtime.emit(loggingEventName, {
-    source: pkg.name,
-    level: logLevels.spam,
-    value: ['loading _JSON', args],
-  });
-  /**/
-
-  type Handler = (args: Arguments) => Promise<JSONValue>;
-
-  const handlers: Record<string, Handler> = {
-    true: () => Promise.resolve(true),
-    false: () => Promise.resolve(false),
-    string: (args) => Promise.resolve(String(args.shift())),
-    number: (args) => Promise.resolve(Number(args.shift())),
-    array: async (args) => {
-      const result: JSONArray = [];
-      while (args[0] !== 'end-array') {
-        result.push(await loadInputFromJSONCommand(runtime, args));
-      }
-      const end = args.shift();
-      if (end !== 'end-array') {
-        return fail(`invalid _JSON command data: unexpected end of array: ${end}`);
-      }
-      return result;
-    },
-    object: async (args) => {
-      const result: JSONRecord = {};
-      while (args[0] !== 'end-object') {
-        const key = args.shift();
-        if (typeof key !== 'string') {
-          return fail(`invalid _JSON command object key type "${typeof key}"`); // Lazy. Improve error reporting.
-        }
-        result[key] = await loadInputFromJSONCommand(runtime, args);
-      }
-      const end = args.shift();
-      if (end !== 'end-object') {
-        return fail(`invalid _JSON command data: unexpected end of object: ${end}`);
-      }
-      return result;
-    },
-  } as const;
-
-  const arg = args.shift();
-
-  if (Array.isArray(arg)) {
-    return await readableToString(await runtime.runCommands('', arg));
-  }
-
-  if (typeof arg !== 'string') {
-    console.error('while loading _JSON', [arg, ...args]);
-    return fail(`invalid _JSON command data type "${typeof arg}"`); // Lazy. Improve error reporting.
-  }
-
-  const handler = handlers[arg];
-  if (!handler) {
-    return fail(`invalid _JSON command data entry "${arg}"`); // Lazy. Improve error reporting.
-  }
-
-  return handler(args);
-}
-
 function loadCommandsImpl(parent: Arguments, root: Arguments, commands: CommandList): Arguments | string {
   if (typeof commands === 'string' || Number.isFinite(commands)) {
     root.push(String(commands)); // Lazy. Push numbers as is, accept any number.
@@ -147,29 +33,38 @@ function loadCommandsImpl(parent: Arguments, root: Arguments, commands: CommandL
   }
 
   if (Array.isArray(commands)) {
+    // Lazy. Hack to workaround _DATA and _JSON creating empty arrays.
+    // Refactor loading code to a proper idiomatic tree walking to handle optional child creation.
+    if (commands.length === 0) {
+      root.push(commands);
+      return root;
+    }
     // Nested call
     const child: Arguments = [];
     for (const command of commands) {
       loadCommandsImpl(root, child, command);
     }
-    root.push(child);
+    if (child.length > 0) {
+      root.push(child);
+    }
     return root;
   }
 
   if (typeof commands === 'object' && commands !== null) {
-    for (const [command, args] of Object.entries(commands)) {
-      if (command === '_DATA') {
-        // Lazy. Seems obsolete, consider retiring in favor of _JSON
-        parent.push(JSON.stringify(args));
-        continue;
-      }
+    const entries = Object.entries(commands);
 
-      if (command === '_JSON') {
-        root.push(command);
-        loadYAMLAsJSONCommand(root, args);
-        continue;
-      }
+    if (entries.length === 1 && entries[0][0] === '_DATA') {
+      parent.push(JSON.stringify(entries[0][1]));
+      return root;
+    }
 
+    if (entries.length === 1 && entries[0][0] === '_JSON') {
+      root.push('echo'); // Lazy. This should create a dependency on or-q/plugin-echo, not on the whole core
+      root.push(JSON.stringify(entries[0][1]));
+      return root;
+    }
+
+    for (const [command, args] of entries) {
       root.push(String(command));
       if (Array.isArray(args)) {
         // Several arguments
@@ -348,16 +243,24 @@ const plugin: Plugin = {
     },
     // See also special handling in loader.
     _DATA: {
-      description: 'converts remaining program to JSON data in input',
-      run: async (_input: string | Readable, args: Arguments, _runtime: IPluginRuntime): Promise<string | Readable> => {
-        return JSON.stringify(args.splice(0, args.length));
+      description: 'not a command, reserved to use _DATA in yaml-scripts to create JSON arguments',
+      run: async (
+        _input: string | Readable,
+        _args: Arguments,
+        _runtime: IPluginRuntime
+      ): Promise<string | Readable> => {
+        return fail('_DATA is not a command');
       },
     },
     // See also special handling in loader.
     _JSON: {
-      description: 'replaces input with special format argument serialized as JSON',
-      run: async (_input: string | Readable, args: Arguments, runtime: IPluginRuntime): Promise<string | Readable> => {
-        return JSON.stringify(await loadInputFromJSONCommand(runtime, args));
+      description: 'not a command, reserved to use _JSON in yaml-scripts to create JSON inputs',
+      run: async (
+        _input: string | Readable,
+        _args: Arguments,
+        _runtime: IPluginRuntime
+      ): Promise<string | Readable> => {
+        return fail('_DATA is not a command');
       },
     },
   },

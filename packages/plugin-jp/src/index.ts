2011608f4fb1a69fca95f20ea45dd894c92d6c86
Alexander Gladysh <agladysh@gmail.com>
2025-08-23 19:05:35 +0300

    refactor(jp): ported to IProgram

--- packages/plugin-jp/src/index.ts
+++ packages/plugin-jp/src/index.ts
@@ -1,5 +1,5 @@
-import type { Arguments, IPluginRuntime, Plugin } from '@or-q/lib';
-import { commandArgument, loadModuleAssets, spawnText } from '@or-q/lib';
+import type { IProgram, Plugin } from '@or-q/lib';
+import { loadModuleAssets, spawnText } from '@or-q/lib';
 import type { Readable } from 'node:stream';
 import pkg from '../package.json' with { type: 'json' };
 
@@ -10,8 +10,9 @@ const plugin: Plugin = {
   commands: {
     jp: {
       description: "run JMSEPath's jp command",
-      run: async (input: string | Readable, args: Arguments, runtime: IPluginRuntime): Promise<string | Readable> => {
-        const query = await commandArgument(runtime, args.shift(), 'usage: jp "[JMSEPath query string]"');
+      run: async (input: string | Readable, program: IProgram): Promise<string | Readable> => {
+        const usage = 'usage: jp "[JMSEPath query string]"';
+        const query = await program.ensureNext(usage).toString();
         return spawnText('jp', input, { args: [query] });
       },
     },
